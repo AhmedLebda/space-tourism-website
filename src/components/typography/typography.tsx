1716b3e3e82feb92a36731ebbc22bf66eb291351
Ahmed Lebda <59537597+AhmedLebda@users.noreply.github.com>
2024-11-03 16:25:46 +0200

    refactor(home): improve layout

---
+++ src/components/typography/typography.tsx
@@ -0,0 +1,53 @@
+import { ElementType } from "react";
+
+type Variant =
+    | "h1"
+    | "h2"
+    | "h3"
+    | "h4"
+    | "h5"
+    | "h6"
+    | "body"
+    | "body-small"
+    | "small"
+const tags: Record<Variant, ElementType> = {
+    h1: "h1",
+    h2: "h2",
+    h3: "h3",
+    h4: "h4",
+    h5: "h5",
+    h6: "h6",
+    body: "p",
+    'body-small': "p",
+    small: "span",
+};
+
+const sizes: Record<Variant, string> = {
+    h1: "text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl",
+    h2: "text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl",
+    h3: "text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl",
+    h4: "text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl",
+    h5: "text-lg sm:text-xl md:text-2xl lg:text-2xl xl:text-3xl 2xl:text-3xl",
+    h6: "text-base sm:text-lg md:text-xl lg:text-xl xl:text-2xl 2xl:text-2xl",
+    body: "text-base sm:text-lg md:text-lg lg:text-lg xl:text-xl 2xl:text-xl",
+    "body-small": "text-sm sm:text-base md:text-base lg:text-base xl:text-lg 2xl:text-xl",
+    small: "text-xs sm:text-sm md:text-base lg:text-base xl:text-lg 2xl:text-lg",
+};
+
+
+
+interface Props {
+    variant: Variant;
+    className?: string;
+    children: React.ReactNode;
+    as?: ElementType;
+}
+
+const Typography = ({ variant, className, children, as: Component = "p" }: Props) => {
+    const sizeClasses = sizes[variant];
+    const Tag = Component || tags[variant];
+
+    return <Tag className={`${sizeClasses} ${className}`}>{children}</Tag>
+};
+
+export default Typography;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,20 @@
 import ExploreButton from "../components/explore-button";
+import Typography from "../components/typography/typography";
 
 function Home() {
-    return <section className="h-full grid gap-4 grid-cols-1 md:grid-cols-2 text-center md:text-left  md:place-items-center md:p-6">
-        <div className="uppercase sm:text-lg leading-relaxed flex flex-col gap-y-4">
-            <h1 className="text-xl xs:text-2xl ">so, you want to travel to <span className="font-bellefair text-8xl xs:text-9xl mt-4 block">space</span></h1>
-            <p>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+    return <section className="h-full grid gap-y-14 grid-cols-1 grid-rows-[auto_1fr] md:grid-rows-1 md:grid-cols-2 text-center md:text-left  md:place-items-center md:px-4 mx-auto max-w-screen-lg ">
+        <article className="flex flex-col gap-y-4 px-6">
+            <Typography variant="h6" as="h1" className="uppercase">
+                so, you want to travel to
+                <Typography variant="h1" as="span" className="font-bellefair block mt-4">
+                    space
+                </Typography>
+            </Typography>
 
-        </div>
+            <Typography variant="body-small" as="p">
+                Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!
+            </Typography>
+        </article>
         <div>
             <ExploreButton />
         </div>
