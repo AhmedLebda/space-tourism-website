a05f1d2a617e6191986beec150014f1e22b4d64e
Ahmed Lebda <59537597+AhmedLebda@users.noreply.github.com>
2024-11-04 20:13:30 +0200

    refactor(technology): improve layout

--- src/components/page_title.tsx
+++ src/components/page_title.tsx
@@ -4,7 +4,7 @@ interface PageTitleProps { number: number, text: string }
 
 const PageTitle = ({ number, text }: PageTitleProps) => {
     return (
-        <Typography variant="h6" as="h1" className='uppercase mx-6'>
+        <Typography variant="h6" as="h1" className='uppercase mx-6 mb-6'>
 
             <Typography variant="h6" as="small" aria-hidden="true" className='mr-2 text-white/50'>
                 {number.toString().padStart(2, '0')}
--- src/pages/Technology.tsx
+++ src/pages/Technology.tsx
@@ -2,6 +2,7 @@
 import { useParams } from "react-router-dom";
 import { technologies } from "../data/technologies"
 import NumberedPagination from "../components/numbered_pagination";
+import Typography from "../components/typography/typography";
 
 const techNames = technologies.map(technology => technology.name);
 
@@ -11,22 +12,35 @@ const Technology = () => {
 
 
     return (
-        <div className="h-full grid gap-4 grid-cols-1 md:grid-cols-2 text-center md:text-left ">
+        // <div className="h-full grid gap-4 grid-cols-1 md:grid-cols-2 text-center md:text-left ">
+        <section className="h-full grid gap-y-6 grid-cols-1 grid-rows-[auto_1fr] md:grid-rows-1 md:grid-cols-2 text-center md:text-left place-items-center mx-auto max-w-screen-xl">
+
             <div className="h-full p-8 flex flex-col md:flex-row gap-6 self-start items-center">
                 <NumberedPagination items={techNames} path="/technology" />
-                <div className="flex flex-col gap-4">
-                    <span className="text-white/50 text-xl md:text-2xl lg:text-3xl uppercase">the terminology...</span>
-                    <h3 className="text-2xl md:text-4xl lg:text-5xl">{currentTechnology.name}</h3>
-                    <p className="md:text-lg">{currentTechnology.description}</p>
-                </div>
+                <article className="flex flex-col gap-6">
+                    <Typography variant="h5" as="h1" className="text-white/60 uppercase font-bellefair tracking-wide">
+                        the terminology...
+                    </Typography>
+
+                    <Typography variant="h2" as="body" className="uppercase font-bellefair">
+                        {currentTechnology.name}
+                    </Typography>
+
+                    <Typography variant="body" as="body" className="mb-10">
+                        {currentTechnology.description}
+                    </Typography>
+                </article>
             </div>
-            <div className="row-start-1 md:col-start-2 self-center">
+
+            <div className="row-start-1 md:col-start-2">
                 <picture>
                     <source srcSet={currentTechnology.images.landscape} media="(max-width: 765px)" />
-                    <img src={currentTechnology.images.portrait} alt={currentTechnology.name} className="h-full md:h-max" />
+                    <img src={currentTechnology.images.portrait} alt={currentTechnology.name} />
                 </picture>
             </div>
-        </div>
+
+        </section>
+        // </div>
     )
 }
 
